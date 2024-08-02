862585c4a2e0ac42c32d2455b6032e79343ba3f5
Olabanjo <49346435+banjo09@users.noreply.github.com>
2024-08-02 17:46:47 +0100

    add cors options

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,8 +9,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(loggerMiddleware);
 
